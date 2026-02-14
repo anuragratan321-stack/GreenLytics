4b84816235a4d1a6a55b91187c4354f3827748ed
anuragratan321-stack <anuragratan321@gmail.com>
2026-02-15 02:11:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const sidebar = document.querySelector(".sidebar");
+const links = document.querySelectorAll(".sidebar a");
+const hamburger = document.getElementById("hamburger");
+const signin = document.getElementById("signIn");
+const overlay = document.querySelector(".overlay");
+const loginCard = document.querySelector(".loginCard");
+const signinDesktop = document.getElementById("signInDesktop");
+
+signinDesktop.addEventListener("click", toggleLogin);
+hamburger.addEventListener("click", toggleMenu);
+signin.addEventListener("click", toggleLogin);
+overlay.addEventListener("click", toggleLogin);
+
+function toggleMenu() {
+    sidebar.classList.toggle("active");
+}
+
+function toggleLogin(e) {
+    if (e) e.preventDefault(); // prevent # link jump
+    loginCard.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        sidebar.classList.remove("active");
+    });
+});
